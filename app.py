1627d147280ac4854adb99449b4406a278a5149b
Anna Jimenez <jimenezpetrushka44@gmail.com>
2026-09-19 22:46:12 -0600

    iniciando index

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-﻿from flask import Flask 
+﻿from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 
 def calculator():
-    return "<h1>Hello estoy probando flask soy annita</h1>"
+    return render_template("index.html")
